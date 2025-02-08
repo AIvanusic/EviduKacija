c0674729aeb976ad1e2f07419b3ca94e5ffdafdb
AIvanusic <you@example.com>
2025-02-08 17:13:31 +0100

    logo za vježbu

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,15 @@
 <template>
   <q-page class="flex flex-center">
     <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+      alt="EviduKacija logo"
+      src="src\assets\logo.png"
+      style="width: 600px; height: 600px"
+    />
   </q-page>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
